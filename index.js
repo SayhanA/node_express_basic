c6455519b48dba9fede5a03d0a607dca62fad181
Sayhan Ahmed <sayhan@ewnbd.com>
2024-07-26 11:55:22 +0600

    Node Exptess basic project

---
+++ index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const rootDir = require('./utils/path');
+
+const port = 4000;
+const app = express();
+
+const adminRouters = require('./routes/admin');
+const shopRouters = require('./routes/shop');
+
+app.use(bodyParser.urlencoded())
+
+// Routing
+app.use('/admin', adminRouters);
+app.use(shopRouters);
+
+// Accesing External file system
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
